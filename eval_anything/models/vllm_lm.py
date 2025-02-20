b82c8978a8dc2d9fad6ab2ceaf0b22776edf2953
Reindulger <reindulger@gmail.com>
2025-02-20 14:49:46 +0800

    Fix the code

--- eval_anything/models/vllm_lm.py
+++ eval_anything/models/vllm_lm.py
@@ -12,23 +12,22 @@ from eval_anything.models.base_model import BaseModel
 from eval_anything.utils.utils import get_messages
 
 class vllmLM(BaseModel):
-    def __init__(self, model_cfgs: Dict[str, Any], vllm_cfgs, infer_cfgs, **kwargs):
-        self.vllm_cfgs_sp, self.vllm_cfgs_llm = vllm_cfgs.SamplingParams, vllm_cfgs.LLM
+    def __init__(self, model_cfgs: Dict[str, Any], infer_cfgs, **kwargs):
         self.model_cfgs = model_cfgs
         self.infer_cfgs = infer_cfgs
-        self.sp_n = self.vllm_cfgs_sp.n
-        self.sp_top_k = self.vllm_cfgs_sp.top_k
-        self.sp_top_p = self.vllm_cfgs_sp.top_p
-        self.sp_temperature = self.vllm_cfgs_sp.temperature
+        self.sp_n = self.infer_cfgs.n
+        self.sp_top_k = self.infer_cfgs.top_k
+        self.sp_top_p = self.infer_cfgs.top_p
+        self.sp_temperature = self.infer_cfgs.temperature
         self.sp_max_tokens = self.infer_cfgs.model_max_length
-        self.sp_prompt_logprobs = self.vllm_cfgs_sp.prompt_logprobs
-        self.sp_logprobs = self.vllm_cfgs_sp.logprobs
+        self.sp_prompt_logprobs = self.infer_cfgs.prompt_logprobs
+        self.sp_logprobs = self.infer_cfgs.logprobs
 
-        self.llm_tokenizer_mode = self.vllm_cfgs_llm.tokenizer_mode
-        self.llm_trust_remote_code = self.vllm_cfgs_llm.trust_remote_code
-        self.llm_gpu_memory_utilization = self.vllm_cfgs_llm.gpu_memory_utilization
-        self.llm_max_num_seqs = self.vllm_cfgs_llm.max_num_seqs
-        tensor_ps = self.vllm_cfgs_llm.tensor_parallel_size
+        self.llm_tokenizer_mode = self.infer_cfgs.tokenizer_mode
+        self.llm_trust_remote_code = self.infer_cfgs.trust_remote_code
+        self.llm_gpu_memory_utilization = self.infer_cfgs.gpu_memory_utilization
+        self.llm_max_num_seqs = self.infer_cfgs.max_num_seqs
+        tensor_ps = self.infer_cfgs.tensor_parallel_size
         self.llm_tensor_parallel_size = tensor_ps if tensor_ps else cuda_device_count_stateless()
 
         self.model_id = self.model_cfgs.model_id
